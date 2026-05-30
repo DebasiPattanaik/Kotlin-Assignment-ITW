c553722826ce3ad75cdf438b0b3a409447be103c
DebasiPattanaik <161007321+DebasiPattanaik@users.noreply.github.com>
2026-05-30 17:50:33 +0530

    Data Class

---
+++ q6.kt
@@ -0,0 +1,11 @@
+data class User
+(
+	val id: Int,
+    val username: String,
+    val isActive: Boolean
+)
+fun main()
+{
+    val originalUser = User(id = 1001, username = "Debasi Pattnaik", isActive = true)
+    println("Original: $originalUser")
+}
